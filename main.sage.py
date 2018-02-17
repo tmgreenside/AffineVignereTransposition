7ecc0fbd8a852426524b9ff6f65dc6691ab3c1c7
tmgreenside <trevmg7@gmail.com>
2018-02-17 13:49:17 -0800

    Added function declarations from De Palma's website instructions

--- main.sage.py
+++ main.sage.py
@@ -28,22 +28,28 @@ def key_gen_vig(key_length):
 
 # reads and encrypts plain_txt using the vigenere cipher and key. writes the
 # output to the file cipher_txt
-def enc_vig(key,plain_txt, cipher_txt):
+def enc_vig(key, plain_txt, cipher_txt):
     return
 
 # reads and decrypts cipher_txt using the vigenere cipher and key. writes
 # the output to the file plain_txt
-def dec_vig(key,cipher_txt, plain_txt):
+def dec_vig(key, cipher_txt, plain_txt):
     return
 
 # returns a randomly generated upper-case alphabet
 def key_gen_trans():
+    alphabet = ""
+    for letter in range(26):
+
     return
 
-# reads and encrypts plain_txt using the alphabetic transposition cipher and key and writes the output to the file cipher_txt.  A dictionary will be useful.
-def enc_trans(key,plain_txt, cipher_txt):
+# reads and encrypts plain_txt using the alphabetic transposition cipher and
+# key and writes the output to the file cipher_txt.  A dictionary will be
+# useful.
+def enc_trans(key, plain_txt, cipher_txt):
     return
 
-# reads and decrypts cipher_txt using the alphabetic transpositon cipher and key and writes the output to the file plain_txt
-def dec_trans(key,cipher_txt,plain_txt):
+# reads and decrypts cipher_txt using the alphabetic transpositon cipher and
+# key and writes the output to the file plain_txt
+def dec_trans(key, cipher_txt, plain_txt):
     return
