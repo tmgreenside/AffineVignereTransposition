f7b39f7b74ac8bfe5dde0d53229df76f7aa38b1c
tmgreenside <trevmg7@gmail.com>
2018-02-19 14:40:37 -0800

    Added main.tokenize() and first two methods, though incomplete, for vig.py

--- main.sage.py
+++ main.sage.py
@@ -6,8 +6,35 @@ from vig import *
 #plain_txt is raw_file with alpha chars transformed to uppercase, white space
 #removed, and eol inserted every fifty characters.
 def tokenize(raw_file, plain_txt):
+    raw = open(raw_file,"r")
+    plain = open(plain_txt, "w")
+    chars = 0
+    writeline = ""
+    while True:
+        line = raw.readline()
+        if not line:
+            break
+        for char in line:
+            if char.isalpha():
+                writeline += char.upper()
+                chars += 1
+            else:
+                if char != ' ' and char != '_' and char != '\n' and char != '\t':
+                    writeline += char
+                    chars += 1
+            if chars >= 50:
+                writeline += '\n'
+                chars = 0
+    plain.write(writeline)
+    raw.close()
+    plain.close()
     return
 
 
+tokenize("message.txt", "plain_text.txt")
 
-sayHello()
+# Trevor tests
+key = key_gen_vig(5)
+print "Key: " + key
+
+enc_vig(key, "plain_text.txt", "cipher_text.txt")
--- vig.py
+++ vig.py
@@ -1,10 +1,38 @@
+from sage.all_cmdline import *   # import sage library
+import random
+
 # returns a randomly generated vigenere key of length, "key_length".
 def key_gen_vig(key_length):
-    return
+    key = ""
+    for i in range(key_length):
+        while True:
+            letter = chr(random.randint(0,25) + 65)
+            if letter not in key:
+                key += letter
+                break
+    return key
 
 # reads and encrypts plain_txt using the vigenere cipher and key. writes the
 # output to the file cipher_txt
 def enc_vig(key, plain_txt, cipher_txt):
+    plain = open(plain_txt, "r")
+    cipher = open(cipher_txt, "w")
+    lenKey = len(key)
+    indKey = 0 # index in key string
+    while True:
+        line = plain.readline()
+        cryptoline = ""
+        if not line:
+            break
+        for char in line:
+            if char.isalpha():
+                cryptoline += chr((ord(char) + ord(key[indKey])) // 26)
+                indKey = (indKey + 1) // lenKey
+            else:
+                cryptoline += char
+        cipher.write(cryptoline)
+    plain.close()
+    cipher.close()
     return
 
 # reads and decrypts cipher_txt using the vigenere cipher and key. writes
